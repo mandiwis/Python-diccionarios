6d4077eae0dc8520ab256e403d402e9449e585dd
mandiwis <145918642+mandiwis@users.noreply.github.com>
2023-11-05 22:47:08 -0300

    Juego highlow

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import discord
+import random
+import asyncio
 
 from discord.ext import commands
 from bot_logic import gen_pass, gen_nintendo, gen_meme
@@ -22,4 +24,32 @@ async def meme(ctx):
 async def nintendo(ctx):
     await ctx.send(gen_nintendo())
 
-bot.run("MTE2MDU5MTYyMjQyMjE0MzAzNg.GwI70i.nfRi7xyO3kBpcppt7NaL8j-BG_b-_b--M215LY")
+@bot.command(name="highlow")
+async def highlow(ctx):
+    numeros = [1,2,3,4,5,6,7,8,9,10]
+    num1 = numeros.pop(random.randint(0,9))
+    num2 = random.choice(numeros)
+
+    usuario = ctx.message.author.name
+    texto_juego1 = f"Hola, {usuario}. Tienes que adivinar si el siguiente número al que voy a poner es mayor o menor que el primero."
+    texto_juego2 = f"{usuario}, el primer número es {num1}. El segundo es \"mayor\" o \"menor\"?"
+    await ctx.send(texto_juego1)
+    await ctx.send(texto_juego2)
+    
+    def is_correct(m):
+        return m.author == ctx.message.author and m.content in ["menor", "mayor"]
+    
+    try:
+        guess = await bot.wait_for('message', check=is_correct, timeout=10.0)
+    except asyncio.TimeoutError:
+        return ctx.send(f'Te demoraste mucho. El número era {num2}.')
+
+    if guess.content == "mayor" and num2 > num1:
+        await ctx.send(f'¡Adivinaste! El segundo número era {num2}')
+    elif guess.content == "menor" and num2 < num1:
+        await ctx.send(f'¡Adivinaste! El segundo número era {num2}')       
+    else:
+        await ctx.send(f'Te equivocaste... El segundo número era {num2} :cry: :sob: :sob:.')
+
+
+bot.run("TOKEN")
